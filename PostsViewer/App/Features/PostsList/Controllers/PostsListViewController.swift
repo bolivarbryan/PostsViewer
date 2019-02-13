150802e36b8f3bf121144cba8a66bab9eb2d3e2f
Bryan A <bolivarbryan@gmail.com>
2019-02-13 15:34:51 -0500

    added segmented control

--- PostsViewer/App/Features/PostsList/Controllers/PostsListViewController.swift
+++ PostsViewer/App/Features/PostsList/Controllers/PostsListViewController.swift
@@ -7,6 +7,22 @@ class PostsListViewController: UIViewController {
 
     let viewModel = PostListViewModel()
     let tableView = UITableView(frame: .zero)
+
+    var deleteButton: UIButton = {
+        let button = UIButton(frame: .zero)
+        button.backgroundColor = .red
+        button.setTitle(Language.deleteAll.localized.capitalized, for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 17, weight: .bold)
+        return button
+    }()
+
+    var segmentedControl: UISegmentedControl = {
+        let control = UISegmentedControl(items: [Language.all.localized.capitalized,
+                                                 Language.favorite.localized.pluralized.capitalized])
+        control.tintColor = .customGreen
+        return control
+    }()
+
     let disposeBag = DisposeBag()
 
     override func viewDidLoad() {
@@ -14,20 +30,44 @@ class PostsListViewController: UIViewController {
         configureUI()
         viewModel.fetchPosts()
         title = Language.post.localized.pluralized.capitalized
+        view.backgroundColor = .gray250
     }
 
     func configureUI() {
+
+        //Delete Button
+        view.addSubview(deleteButton)
+        deleteButton.snp.makeConstraints {
+            $0.bottom.right.left.equalToSuperview()
+            $0.height.equalTo(80)
+        }
+
+        //Segmented Control
+        view.addSubview(segmentedControl)
+        segmentedControl.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(10)
+            $0.right.equalToSuperview().offset(-10)
+            $0.left.equalToSuperview().offset(10)
+            $0.height.equalTo(40)
+        }
+        segmentedControl.selectedSegmentIndex = 0
+
+        //TableView
         view.addSubview(tableView)
         tableView.snp.makeConstraints {
-            $0.edges.equalToSuperview()
+            $0.top.equalTo(segmentedControl.snp.bottom).offset(10)
+            $0.right.left.equalToSuperview()
+            $0.bottom.equalTo(deleteButton.snp.top)
         }
 
         tableView.register(PostTableViewCell.self, forCellReuseIdentifier: PostTableViewCell.identifier)
 
+        //Rx Binders
         viewModel.posts.asObservable()
             .bind(to: tableView.rx.items(cellIdentifier: PostTableViewCell.identifier,
                                          cellType: PostTableViewCell.self)) {  row, element, cell in
                                             cell.post.value = element
+                                            cell.accessoryType = .disclosureIndicator
             }
             .disposed(by: disposeBag)
     }
--- PostsViewer/Extensions/UIColor+Style.swift
+++ PostsViewer/Extensions/UIColor+Style.swift
@@ -2,4 +2,7 @@ import UIKit
 
 extension UIColor {
     static let customGreen = #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1)
+    static let gray250 = #colorLiteral(red: 0.9803921569, green: 0.9803921569, blue: 0.9803921569, alpha: 1)
 }
+
+
